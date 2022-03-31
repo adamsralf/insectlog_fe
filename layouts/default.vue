32ddaa445b01b080be247543ed4b8c31ede7911a
NinoKrb <nino.gralla@gmx.de>
2022-03-31 23:22:35 +0200

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Layout/navbar";
+export default {
+  name: "default",
+  components: {Navbar}
+}
+</script>
+
+<style scoped>
+
+</style>
