7971c5ed99391154358435ede3331e1d9de5accf
NinoKrb <nino.gralla@gmx.de>
2022-04-03 23:39:46 +0200

    Add footer to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,16 @@
   <div>
     <Navbar></Navbar>
     <Nuxt/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Navbar from "~/components/Layout/navbar";
+import Navbar from "@/components/Layout/navbar";
+import Footer from "@/components/Layout/footer";
 export default {
   name: "default",
-  components: {Navbar}
+  components: {Footer, Navbar}
 }
 </script>
 
