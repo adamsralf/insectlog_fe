43d279c60e57f869495be5ae7c2a0ac770ce07a8
NinoKrb <nino.gralla@gmx.de>
2022-04-03 23:38:18 +0200

    update layout components

--- components/Layout/navbar.vue
+++ components/Layout/navbar.vue
@@ -12,15 +12,14 @@
         <div class="-mr-2 -my-2 md:hidden">
           <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500" aria-expanded="false">
             <span class="sr-only">Open menu</span>
-            <!-- Heroicon name: outline/menu -->
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <svg @click="phone_navbar = true" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
         </div>
         <div class="hidden md:flex-1 md:flex md:items-center md:justify-between">
           <nav class="flex space-x-10">
-            <div v-for="link in topbar.links" :key="link.url">
+            <div v-for="link in topbar.links" :key="link.url + 'desktop'">
               <div>
                 <div class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
                   <a v-if="link.external" :href="link.url" target="_blank">
@@ -45,27 +44,16 @@
       </div>
     </div>
 
-    <!--
-      Mobile menu, show/hide based on mobile menu state.
-
-      Entering: "duration-200 ease-out"
-        From: "opacity-0 scale-95"
-        To: "opacity-100 scale-100"
-      Leaving: "duration-100 ease-in"
-        From: "opacity-100 scale-100"
-        To: "opacity-0 scale-95"
-    -->
-    <div class="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+    <div class="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden" v-if="phone_navbar">
       <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
         <div class="pt-5 pb-6 px-5 sm:pb-8">
           <div class="flex items-center justify-between">
             <div>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-primary-600.svg" alt="Workflow">
+              <img class="h-8 w-auto" :src="require('~/assets/images/logo/' + topbar.logo)" alt="" v-if="topbar.logo">
             </div>
             <div class="-mr-2">
-              <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500">
+              <button @click="phone_navbar = false" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500">
                 <span class="sr-only">Close menu</span>
-                <!-- Heroicon name: outline/x -->
                 <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
@@ -75,71 +63,34 @@
           <div class="mt-6 sm:mt-8">
             <nav>
               <div class="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
-                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                  <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-primary-500 text-white sm:h-12 sm:w-12">
-                    <!-- Heroicon name: outline/chart-bar -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                    </svg>
-                  </div>
-                  <div class="ml-4 text-base font-medium text-gray-900">Analytics</div>
-                </a>
-
-                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                  <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-primary-500 text-white sm:h-12 sm:w-12">
-                    <!-- Heroicon name: outline/cursor-click -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-                    </svg>
-                  </div>
-                  <div class="ml-4 text-base font-medium text-gray-900">Engagement</div>
-                </a>
-
-                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                  <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-primary-500 text-white sm:h-12 sm:w-12">
-                    <!-- Heroicon name: outline/shield-check -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                    </svg>
-                  </div>
-                  <div class="ml-4 text-base font-medium text-gray-900">Security</div>
-                </a>
-
-                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                  <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-primary-500 text-white sm:h-12 sm:w-12">
-                    <!-- Heroicon name: outline/view-grid -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                    </svg>
-                  </div>
-                  <div class="ml-4 text-base font-medium text-gray-900">Integrations</div>
-                </a>
-              </div>
-              <div class="mt-8 text-base">
-                <a href="#" class="font-medium text-primary-600 hover:text-primary-500"> View all products <span aria-hidden="true">&rarr;</span></a>
+                <div v-for="link in topbar.links" :key="link.url + 'desktop'">
+                  <a v-if="link.external" :href="link.url" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
+                    <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-primary-500 text-white sm:h-12 sm:w-12">
+                      <div v-html="link.icon"></div>
+                    </div>
+                    <div class="ml-4 text-base font-medium text-gray-900">{{ link.title }}</div>
+                  </a>
+                  <nuxt-link v-else :to="link.url" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
+                    <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-primary-500 text-white sm:h-12 sm:w-12">
+                      <div v-html="link.icon"></div>
+                    </div>
+                    <div class="ml-4 text-base font-medium text-gray-900">{{ link.title }}</div>
+                  </nuxt-link>
+                </div>
               </div>
             </nav>
           </div>
         </div>
         <div class="py-6 px-5">
-          <div class="grid grid-cols-2 gap-4">
-            <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Pricing </a>
-
-            <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Docs </a>
-
-            <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Company </a>
-
-            <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Resources </a>
-
-            <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Blog </a>
-
-            <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Contact Sales </a>
-          </div>
           <div class="mt-6">
-            <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-primary-600 hover:bg-primary-700"> Sign up </a>
+            <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-primary-600 hover:bg-primary-700">
+              Registrieren
+            </a>
             <p class="mt-6 text-center text-base font-medium text-gray-500">
-              Existing customer?
-              <a href="#" class="text-primary-600 hover:text-primary-500"> Sign in </a>
+              Du hast bereits einen Account?
+              <a href="#" class="text-primary-600 hover:text-primary-500">
+                Anmelden
+              </a>
             </p>
           </div>
         </div>
@@ -150,9 +101,10 @@
 
 <script>
 export default {
-  name: "navbar",
+  name: "LayoutNavbar",
   data() {
     return {
+      phone_navbar: false,
       topbar: {
         links: [],
         buttons: {},
