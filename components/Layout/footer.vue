ebb1c5458847fbbd652282c285b09f42bc2df175
NinoKrb <nino.gralla@gmx.de>
2022-04-05 01:26:50 +0200

    Update footer
    Add Project owner tag

--- components/Layout/footer.vue
+++ components/Layout/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-white">
-    <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+    <p class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
       <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
         <div class="px-5 py-2">
           <a href="#" class="text-base text-gray-500 hover:text-primary"> Projekt </a>
@@ -37,7 +37,13 @@
           </svg>
         </a>
       </div>
-      <p class="mt-8 text-center text-base text-gray-400">&copy; 2022 Technische Berufliche Schule 1. All rights reserved.</p>
+    <div>
+      <p class="mt-8 text-center text-base text-gray-400">
+        &copy; 2022 Technische Berufliche Schule 1. All rights reserved.
+      </p>
+      <p class="mt-2 mb-8 text-center text-base text-gray-400">
+        Powered by Erasmus+
+      </p>
     </div>
   </footer>
 </template>
