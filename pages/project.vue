aa1de08d3dee7c4e37fc717ca463f2010e15289a
NinoKrb <nino.gralla@gmx.de>
2022-04-17 02:01:11 +0200

    Implement new project page components

--- pages/project.vue
+++ pages/project.vue
@@ -1,33 +1,35 @@
 <template>
-  <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
-    <div class="text-center">
-      <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-        <span class="block xl:inline">Hier entsteht die</span>
-        <span class="block text-primary-600 xl:inline">Projektübersicht</span>
-      </h1>
-      <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-      <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-        <div class="rounded-md shadow">
-          <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary-600 hover:bg-primary-700 md:py-4 md:text-lg md:px-10">
-            Button 1
-          </a>
-        </div>
-        <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-          <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-primary-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
-            Button 2
-          </a>
-        </div>
-      </div>
-    </div>
-  </main>
+  <div>
+    <ProjectHero :content="project.hero"></ProjectHero>
+    <ProjectStatistics :content="project.statistics"></ProjectStatistics>
+    <ProjectFeatures :content="project.features"></ProjectFeatures>
+    <ProjectTeam :content="project.team"></ProjectTeam>
+  </div>
 </template>
 
 <script>
+import ProjectHero from "@/components/Project/hero";
+import ProjectFeatures from "@/components/Project/features";
 export default {
-  name: "project"
+  name: "project",
+  components: { ProjectHero, ProjectFeatures },
+  data() {
+    return {
+      project: {
+        hero: null,
+        statistics: null,
+        team: null,
+        features: null
+      }
+    }
+  },
+  async fetch() {
+    let content = await this.$content(this.$i18n.locale, 'project').fetch()
+    this.project.hero = content.hero
+    this.project.statistics = content.statistics
+    this.project.team = content.team
+    this.project.features = content.features
+  },
 }
 </script>
 
-<style scoped>
-
-</style>
