8acc08bb1b1f77f6156296f28b45b7577c52d39d
NinoKrb <nino.gralla@gmx.de>
2022-04-18 22:25:16 +0200

    Create navigation links to auth pages for dashboard

--- pages/register.vue
+++ pages/register.vue
@@ -34,9 +34,9 @@
               </div>
 
               <div>
-                <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+                <nuxt-link to="/dashboard" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
                   {{ $t('register.register') }}
-                </button>
+                </nuxt-link>
               </div>
             </form>
           </div>
