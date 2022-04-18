59c9f800e1fe50d6f052e19e34ce2865d87a81de
NinoKrb <nino.gralla@gmx.de>
2022-04-18 22:25:40 +0200

    Create simple dashboard layout

---
+++ layouts/dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Navbar :content="navbar"></Navbar>
+    <Nuxt></Nuxt>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Dashboard/Layout/navbar";
+
+export default {
+  name: "dashboard",
+  components: {Navbar},
+  data() {
+    return {
+      navbar: {},
+    }
+  },
+  async fetch() {
+    let content = await this.$content(this.$i18n.locale, 'dashboard_layout').fetch()
+    this.navbar = content.navbar
+    console.log(content)
+  },
+}
+</script>
