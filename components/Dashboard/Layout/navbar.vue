07574e56772e900ca1c41cb7d736ccc76c3ce6fc
NinoKrb <nino.gralla@gmx.de>
2022-05-05 22:49:10 +0200

    Fix navbar highlight on route change

--- components/Dashboard/Layout/navbar.vue
+++ components/Dashboard/Layout/navbar.vue
@@ -30,7 +30,7 @@
               <div>
                 <button @click="profile_dropdown = !profile_dropdown" type="button" class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span class="sr-only">{{ content.open_user_menu }}</span>
-                  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                  <img class="h-8 w-8 rounded-full object-cover" src="https://avatars.githubusercontent.com/u/66381853?v=4" alt="">
                 </button>
               </div>
 
@@ -105,8 +105,13 @@ export default {
     return {
       profile_dropdown: false,
       mobile_dropdown: false,
-      current_url: this.$nuxt.$route.path
+      current_url: this.$router.currentRoute.path
     }
+  },
+  watch: {
+    $route() {
+      this.current_url = this.$router.currentRoute.path
+    },
   },
   props: {
     content: {
